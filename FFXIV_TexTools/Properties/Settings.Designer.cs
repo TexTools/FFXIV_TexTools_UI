e1e0ce4d4d73b437daa37566e94897ec7430ffae
Liinko <dualwielddevelopment@gmail.com>
2019-02-15 18:06:27 -0600

    Addition of cull mode to settings for retention between model switches

--- FFXIV_TexTools/Properties/Settings.Designer.cs
+++ FFXIV_TexTools/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace FFXIV_TexTools.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -176,6 +176,18 @@ namespace FFXIV_TexTools.Properties {
             }
             set {
                 this["Etc_Color"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Back")]
+        public string Cull_Mode {
+            get {
+                return ((string)(this["Cull_Mode"]));
+            }
+            set {
+                this["Cull_Mode"] = value;
             }
         }
     }
