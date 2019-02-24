dc72b6bb017e0ab246b114f2533275c0f535b723
Liinko <dualwielddevelopment@gmail.com>
2019-02-23 22:46:15 -0600

    Addition of unhandled exception dialog

--- FFXIV_TexTools/App.xaml.cs
+++ FFXIV_TexTools/App.xaml.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using FFXIV_TexTools.Helpers;
 using FFXIV_TexTools.Properties;
 using MahApps.Metro;
+using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Forms;
+using Application = System.Windows.Application;
+using Clipboard = System.Windows.Clipboard;
 
 namespace FFXIV_TexTools
 {
@@ -20,7 +20,28 @@ namespace FFXIV_TexTools
             var appStyle = ThemeManager.DetectAppStyle(Application.Current);
 
             ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent(appStyle.Item2.Name), ThemeManager.GetAppTheme(Settings.Default.Application_Theme));
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
             base.OnStartup(e);
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ver = FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).FileVersion;
+
+            const string lineBreak = "\n======================================================\n";
+
+            var errorText = "TexTools ran into an error.\n\n" +
+                            "Please submit a bug report with the following information.\n " +
+                            lineBreak +
+                            e.ExceptionObject +
+                            lineBreak + "\n" +
+                            "Copy to clipboard?";
+
+            if (FlexibleMessageBox.Show(errorText, "Crash Report " + ver, MessageBoxButtons.YesNo, MessageBoxIcon.Error) == DialogResult.Yes)
+            {
+                Clipboard.SetText(e.ExceptionObject.ToString());
+            }
         }
     }
 }
