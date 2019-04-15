7f97f8e1428a66af1588afd1cc674188981c5970
esrinzou <esrin@qq.com>
2019-04-15 16:38:25 +0800

    for multilingual UI

--- FFXIV_TexTools/Resources/UIStrings.Designer.cs
+++ FFXIV_TexTools/Resources/UIStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace FFXIV_TexTools.Resources {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或移除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 VS 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class UIStrings {
@@ -885,6 +885,15 @@ namespace FFXIV_TexTools.Resources {
         public static string Join_us_on_Discord {
             get {
                 return ResourceManager.GetString("Join_us_on_Discord", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   查找类似 Keep Camera Position 的本地化字符串。
+        /// </summary>
+        public static string Keep_Camera_Position {
+            get {
+                return ResourceManager.GetString("Keep_Camera_Position", resourceCulture);
             }
         }
         
