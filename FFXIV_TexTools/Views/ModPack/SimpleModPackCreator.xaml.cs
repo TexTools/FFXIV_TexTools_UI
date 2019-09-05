23274c75d218ad09b49454fc5a38695e44693506
esrinzou <esrin@qq.com>
2019-09-05 12:15:59 +0800

    Added(SimpleModPackCreator Add filter)

--- FFXIV_TexTools/Views/ModPack/SimpleModPackCreator.xaml.cs
+++ FFXIV_TexTools/Views/ModPack/SimpleModPackCreator.xaml.cs
@@ -96,8 +96,31 @@ namespace FFXIV_TexTools.Views
 
             ModSizeLabel.Content = "0";
             ModListView.IsEnabled = true;
-        }
 
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(ModListView.ItemsSource);
+            view.Filter = NameFilter;
+        }
+        /// <summary>
+        /// filtering ModListView
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        private bool NameFilter(object item)
+        {
+            if (String.IsNullOrEmpty(SearchTextBox.Text.Trim()))
+                return true;
+            else
+                return ((item as SimpleModPackEntries).Name.IndexOf(SearchTextBox.Text.Trim(), StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+        /// <summary>
+        /// Filtering Text
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(ModListView.ItemsSource).Refresh();
+        }
         /// <summary>
         /// Creates the simple mod pack data list
         /// </summary>
