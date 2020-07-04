a3d3787933ae6a5ae41136e53824115f3e36b86d
KoraLyn <koralynffxiv@gmail.com>
2020-07-04 11:48:04 +0200

    Update version to 2.0.12.6

--- FFXIV_TexTools/Properties/AssemblyInfo.cs
+++ FFXIV_TexTools/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FFXIV_TexTools")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.12.5")]
-[assembly: AssemblyFileVersion("2.0.12.5")]
+[assembly: AssemblyVersion("2.0.12.6")]
+[assembly: AssemblyFileVersion("2.0.12.6")]
