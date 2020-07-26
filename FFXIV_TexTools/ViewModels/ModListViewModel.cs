76c7e22458ba8548593b6fe0984e449177618b5d
KoraLyn <koralynffxiv@gmail.com>
2020-07-26 21:51:25 +0200

    Revert "- Skin and Lip Color Influence calculations updated."
    This reverts commit 8b2c745869b67df4618a161b6d9504819430e1e0.

--- FFXIV_TexTools/ViewModels/ModListViewModel.cs
+++ FFXIV_TexTools/ViewModels/ModListViewModel.cs
@@ -546,7 +546,15 @@ namespace FFXIV_TexTools.ViewModels
                          select mod).ToList();
                 }
 
-                tex = new Tex(_gameDirectory);
+                if (modItems.Count > 10)
+                {
+                    tex = new Tex(_gameDirectory, selectedItem.DataFile);
+                    await tex.GetIndexFileDictionary();
+                }
+                else
+                {
+                    tex = new Tex(_gameDirectory);
+                }
 
                 var modNum = 0;
 
