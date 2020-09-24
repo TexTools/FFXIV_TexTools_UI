872b0f69beb9bd2f180d510034180a4e9e82c27c
Sel <admin@xivmodarchive.com>
2020-09-23 23:18:54 -0400

    Updated build scripts.

--- push_master.bat
+++ push_master.bat
@@ -7,36 +7,46 @@ if not exist FFXIV_TexTools.sln (
 )
 
 if "%~1"=="" (
-	SET /P %1= Enter Version number. [In the form of 'v2.x.x.x']: 
-	if  "%~1"=="" (
-		echo Update cancelled.  No version number provided.
-		EXIT
-	)
+	SET /P patchver= Enter Version number. [In the form of 'v2.x.x.x']: 
+) ELSE (
+	set patchver=%1
 )
 
-echo Creating MASTER update %1 for Framework Repo...
+if  %patchver%=="" (
+	echo Update cancelled.  No version number provided.
+	EXIT
+)
+
+echo Creating MASTER update %patchver% for Framework Repo...
+pause
 cd ./lib/xivmoddingframework
 git checkout master
-git merge develop --squash
-git commit -m "Update %1"
-git tag -a %1 -m "Update %1"
+git merge develop --no-ff --no-commit
+git commit -m "Update %patchver%"
+git tag -a %patchver% -m "Update %patchver%"
 git push
 git push --tags
 
-echo Creating MASTER update %1 for UI Repo...
+echo Creating MASTER update %patchver% for UI Repo...
+pause
 cd ../../
 git checkout master
-git merge develop --squash
+git merge develop --no-ff --no-commit
 git add lib/*
-git commit -m "Update %1"
-git tag -a %1 -m "Update %1"
+git commit -m "Update %patchver%"
+git tag -a %patchver% -m "Update %patchver%"
 git push
 git push --tags
 
 echo Returning to Develop branch...
 cd ./lib/xivmoddingframework
 git checkout develop
+git merge master --ff-only 
+git push
+
 cd ../../
 git checkout develop
+git merge master --ff-only
+git push
 
 pause
