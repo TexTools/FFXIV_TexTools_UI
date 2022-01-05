b49b42b69ddd070a15b679ba4385cd240dd01d5a
KoraLyn <koralynffxiv@gmail.com>
2022-01-05 15:20:05 +0100

    Add row number labels to colourset editor

---
+++ FFXIV_TexTools/Views/Controls/ColorsetRowControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FFXIV_TexTools.Controls
+{
+    /// <summary>
+    /// Interaction logic for ColorsetRowControl.xaml
+    /// </summary>
+    public partial class ColorsetRowControl : UserControl
+    {
+        public ColorsetRowControl(int id)
+        {
+            InitializeComponent();
+            RowNumber.Content = (id + 1).ToString();
+            DataContext = id;
+        }
+
+        public ImageSource RowImageSource
+        {
+            get => RowImage.Source;
+            set => RowImage.Source = value;
+        }
+    }
+}
