3ddc56359f408aa9036bf8e5b466e2a5f4dd9aa4
KoraLyn <koralynffxiv@gmail.com>
2022-07-31 21:25:54 +0200

    Update version to v2.3.8.1

--- FFXIV_TexTools/Properties/AssemblyInfo.cs
+++ FFXIV_TexTools/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FFXIV_TexTools")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.7.8")]
-[assembly: AssemblyFileVersion("2.3.7.8")]
+[assembly: AssemblyVersion("2.3.8.1")]
+[assembly: AssemblyFileVersion("2.3.8.1")]
