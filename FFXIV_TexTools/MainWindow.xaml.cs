437eb81b8b54f42f65cffb5f4d2cff2091fc0beb
Lunaretic <Lunaretic@users.noreply.github.com>
2024-04-24 02:17:55 -0400

    Beta and library updates

--- FFXIV_TexTools/MainWindow.xaml.cs
+++ FFXIV_TexTools/MainWindow.xaml.cs
@@ -71,7 +71,7 @@ namespace FFXIV_TexTools
         private FullModelView _fmv;
         public readonly System.Windows.Forms.IWin32Window Win32Window;
 
-        public static readonly string BetaSuffix = " DT8";
+        public static readonly string BetaSuffix = " DT9";
         public static bool IsBetaVersion {
             get
             {
