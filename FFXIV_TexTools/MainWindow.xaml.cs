501b009c5333c6beefe24707f91dc6251d1342b8
Lunaretic <Lunaretic@users.noreply.github.com>
2024-05-08 20:47:52 -0400

    Beta and Lib bump

--- FFXIV_TexTools/MainWindow.xaml.cs
+++ FFXIV_TexTools/MainWindow.xaml.cs
@@ -80,7 +80,7 @@ namespace FFXIV_TexTools
         private FullModelView _fmv;
         public readonly System.Windows.Forms.IWin32Window Win32Window;
 
-        public static readonly string BetaSuffix = "- DT Benchmark Build 16";
+        public static readonly string BetaSuffix = "- DT Benchmark Build 17";
         public static bool IsBetaVersion {
             get
             {
