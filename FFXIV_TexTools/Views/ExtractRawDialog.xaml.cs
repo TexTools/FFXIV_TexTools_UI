5b07b079f9b12c892f6e79864d4e4b9664cd513d
Lunaretic <Lunaretic@users.noreply.github.com>
2024-05-10 22:25:27 -0400

    Minor name change on a function to be consistent with other overloads.

--- FFXIV_TexTools/Views/ExtractRawDialog.xaml.cs
+++ FFXIV_TexTools/Views/ExtractRawDialog.xaml.cs
@@ -72,7 +72,7 @@ namespace FFXIV_TexTools.Views
 
             try
             {
-                var offset = await tx.GetRawOffset(path);
+                var offset = await tx.GetRawDataOffset(path);
                 var df = IOUtil.GetDataFileFromPath(path);
                 if (offset <= 0)
                 {
