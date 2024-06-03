bd3850d650ae27e5c2d26f9903e04a21f7161897
Lunaretic <Lunaretic@users.noreply.github.com>
2024-06-03 14:57:36 -0400

    ref

--- FFXIV_TexTools/MainWindow.xaml.cs
+++ FFXIV_TexTools/MainWindow.xaml.cs
@@ -87,9 +87,9 @@ namespace FFXIV_TexTools
         public readonly System.Windows.Forms.IWin32Window Win32Window;
 
 #if ENDWALKER
-        public static readonly string BetaSuffix = "- CursedTools Build 25 (ENDWALKER)";
+        public static readonly string BetaSuffix = "- CursedTools Build 26 (ENDWALKER)";
 #else
-        public static readonly string BetaSuffix = "- CursedTools Build 25 (DAWNTRAIL)";
+        public static readonly string BetaSuffix = "- CursedTools Build 26 (DAWNTRAIL)";
 #endif
         public static bool IsBetaVersion {
             get
