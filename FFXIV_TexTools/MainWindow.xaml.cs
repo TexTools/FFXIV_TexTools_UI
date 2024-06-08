65cac74b1310a0d96295a1f162ceb226d6639497
Lunaretic <Lunaretic@users.noreply.github.com>
2024-06-08 18:00:20 -0400

    ref/beta bump.

--- FFXIV_TexTools/MainWindow.xaml.cs
+++ FFXIV_TexTools/MainWindow.xaml.cs
@@ -89,9 +89,9 @@ namespace FFXIV_TexTools
         public readonly System.Windows.Forms.IWin32Window Win32Window;
 
 #if ENDWALKER
-        public static readonly string BetaSuffix = "- CursedTools Build 37 (ENDWALKER)";
+        public static readonly string BetaSuffix = "- CursedTools Build 38 (ENDWALKER)";
 #else
-        public static readonly string BetaSuffix = "- CursedTools Build 37 (DAWNTRAIL)";
+        public static readonly string BetaSuffix = "- CursedTools Build 38 (DAWNTRAIL)";
 #endif
         public static bool IsBetaVersion {
             get
